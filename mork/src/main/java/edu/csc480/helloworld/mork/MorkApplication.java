c39079dd2845198738113806b5c2d6cfdfafc4cc
jnewtonb <jnewtonb@oswego.edu>
2021-02-15 13:36:56 -0500

    Removed

--- mork/src/main/java/edu/csc480/helloworld/mork/MorkApplication.java
+++
@@ -1,9 +0,0 @@
-package edu.csc480.helloworld.mork;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/mork")
-public class MorkApplication extends Application {
-
-}
--- webpage/src/main/java/edu/csc480/helloworld/core/CoreApplication.java
+++
@@ -1,9 +0,0 @@
-package edu.csc480.helloworld.core;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/core")
-public class CoreApplication extends Application {
-
-}
